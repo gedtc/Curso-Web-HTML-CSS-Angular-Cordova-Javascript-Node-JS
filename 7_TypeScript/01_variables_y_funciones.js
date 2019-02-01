a78525853cae1004df8a4ec358f5184372990d91
gedtc <guilletcz@gmail.com>
2019-02-01 12:11:56 +0100

    z

---
+++ 7_TypeScript/01_variables_y_funciones.js
@@ -0,0 +1,6 @@
+var unNumero; /* let = var */
+unNumero = 12;
+console.log("Un número " + unNumero);
+var unTexto = "Un texto";
+console.log(unTexto);
+alert(unTexto);
---
+++ 7_TypeScript/01_variables_y_funciones.ts
@@ -0,0 +1,13 @@
+﻿let unNumero : number;   /* let = var */
+unNumero = 12;
+
+console.log("Un número " + unNumero);
+
+var unTexto = "Un texto";
+
+console.log(unTexto);
+
+alert(unTexto);
+
+
+
