8a4a5421dd69d34e48363f6acdbbd3c8b8794f6b
gedtc <guilletcz@gmail.com>
2019-02-05 10:19:16 +0100

    5 feb descanso

--- 7_TypeScript/01_variables_y_funciones.js
+++
@@ -1,6 +0,0 @@
-var unNumero; /* let = var */
-unNumero = 12;
-console.log("Un número " + unNumero);
-var unTexto = "Un texto";
-console.log(unTexto);
-alert(unTexto);
---
+++ 7_TypeScript/03_ejercicio_camisetas.ts
@@ -0,0 +1,60 @@
+﻿interface IProducto {
+  precio: number;
+  mostrar(): void; // any en TS es un tipo
+
+}
+class Camiseta implements IProducto {
+
+  precio: number;
+  talla: string;
+  color: string;
+
+  constructor(pre: number, tall: string, col: string) {
+    //dentro del constructor son variables locales
+
+    this.talla = tall;
+    this.precio = pre;
+    this.color = col;
+
+  }
+  mostrar() {
+    console.log("Info camisetas: " + this.precio + "€");
+    console.log("Talla: " + this.talla);
+    console.log("Color: " + this.color);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+class Libro implements IProducto {
+
+
+  constructor(public precio: number, public titulo: string) {
+    // public titulo: string  es una forma exclusiva de TS de introducir variables 
+
+
+  }
+  mostrar() {
+    console.log("Info libro: " + this.precio + "€");
+    console.log("Título: " + this.titulo);
+
+
+  }
+}
+
+let miLibro : Libro = new Libro(900, "Mi libro");
+let camisetaJorge : Camiseta;
+camisetaJorge = new Camiseta(17,"L","arcoiris");
+
+let algunProducto : IProducto;
+algunProducto = new Camiseta(19, "S","roja");
+algunProducto.mostrar();
+
+algunProducto = new Libro(32, "KrV");
+algunProducto.mostrar();
+algunProducto =miLibro;
+algunProducto.mostrar();
+
+
+//Cuando dejamos una función vacía TS infiere el tipo a partir del return.
+//También ocurre con variables.
